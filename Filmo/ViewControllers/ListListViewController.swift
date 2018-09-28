a27a714f83b1029bd6ada2e9ddc06329eb7e2694
Benjamin Frost <bf@ion.studio>
2018-09-28 11:11:44 +1000

    Can now search for films using the search tab and add them to lists.

--- Filmo/ViewControllers/ListListViewController.swift
+++ Filmo/ViewControllers/ListListViewController.swift
@@ -9,13 +9,21 @@
 import UIKit
 
 class ListListViewControllerTableViewCell: UITableViewCell {
-    
     @IBOutlet weak var lblTitle: UILabel?
+}
+
+
+protocol ListListViewControllerDelegate: AnyObject {
+    func listList(viewController: ListListViewController, selectedList: FilmListReference)
+    func listList(viewController: ListListViewController, decorateCell: ListListViewControllerTableViewCell, forList: FilmListReference)
+    func listList(viewController: ListListViewController, pressedAdd: UIBarButtonItem?)
 }
 
 class ListListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView?
+    
+    var delegate: ListListViewControllerDelegate? = nil
     
     var lists: [FilmListReference]? = nil {
         didSet {
@@ -25,10 +33,13 @@ class ListListViewController: UIViewController, UITableViewDataSource, UITableVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        ServiceProvider().backend.getFilmListReferences().reportProgress().done { [weak self] (lists) in
-            self?.lists = lists
-        }
+        self.tableView?.tableFooterView = UIView()
+    }
+    
+    func listFor(indexPath: IndexPath) -> FilmListReference? {
+        guard let lists = self.lists else { return nil }
+        guard indexPath.row < lists.count else { return nil }
+        return lists[indexPath.row]
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -41,10 +52,11 @@ class ListListViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let list = self.lists?[indexPath.row] else { return UITableViewCell() }
+        guard let list = self.listFor(indexPath: indexPath) else { return UITableViewCell() }
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListListViewControllerTableViewCell else { return UITableViewCell() }
         
-        cell.lblTitle?.text = list.name ?? list.id
+        cell.lblTitle?.text = list.name
+        self.delegate?.listList(viewController: self, decorateCell: cell, forList: list)
         
         return cell
     }
@@ -58,8 +70,28 @@ class ListListViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let list = self.lists?[indexPath.row] else { return }
-        self.performSegue(withIdentifier: "showList", sender: list)
+        self.tableView?.deselectRow(at: indexPath, animated: true)
+        guard let list = self.listFor(indexPath: indexPath) else { return }
+        self.delegate?.listList(viewController: self, selectedList: list)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard let list = self.listFor(indexPath: indexPath) else { return }
+        
+        switch editingStyle {
+            
+        case .delete:
+            ServiceProvider().backend.delete(list: list).lockView(view: self.view).reportProgress().done { [weak self] (_) in
+                self?.lists = self?.lists?.filter({ $0.id != list.id })
+            }.cauterize()
+            
+        default: return
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -70,6 +102,11 @@ class ListListViewController: UIViewController, UITableViewDataSource, UITableVi
             vc.title = list.name
             vc.filmListPromise = ServiceProvider().backend.getFilmList(id: list.id)
         }
+    }
+    
+    
+    @objc func onAddListPressed(sender: Any?) {
+        self.delegate?.listList(viewController: self, pressedAdd: sender as? UIBarButtonItem)
     }
     
     
--- Filmo/ViewControllers/MainTabBarViewController.swift
+++ Filmo/ViewControllers/MainTabBarViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import Toast_Swift
 
-class MainTabBarViewController: UITabBarController {
+
+
+class MainTabBarViewController: UITabBarController, ListListViewControllerDelegate, FilmSearchViewControllerDelegate, FilmDetailsViewControllerDelegate {
     
     var lists: [FilmListReference]? = nil
     
@@ -38,6 +41,15 @@ class MainTabBarViewController: UITabBarController {
         
         listListVc.lists = lists
         listListVc.title = "My Lists"
+        listListVc.delegate = self
+        
+        let button = UIBarButtonItem(
+            barButtonSystemItem: UIBarButtonSystemItem.add,
+            target: listListVc,
+            action: #selector(ListListViewController.onAddListPressed(sender:))
+        )
+        
+        listListVc.navigationItem.setRightBarButton(button, animated: false)
         
         guard let list = lists.first else {
             return nav
@@ -53,7 +65,8 @@ class MainTabBarViewController: UITabBarController {
         
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "search")
         vc?.title = "Search"
-        return vc
+        (vc as? FilmSearchViewController)?.delegate = self
+        return vc.map({ UINavigationController(rootViewController: $0) })
     }
     
     func getFriendsPage() -> UIViewController? {
@@ -68,8 +81,111 @@ class MainTabBarViewController: UITabBarController {
         return vc
     }
     
+    // MARK: - ListListViewControllerDelegate
     
+    func listList(viewController: ListListViewController, selectedList list: FilmListReference) {
+        viewController.performSegue(withIdentifier: "showList", sender: list)
+    }
+    
+    func listList(viewController: ListListViewController, decorateCell cell: ListListViewControllerTableViewCell, forList list: FilmListReference) {
+        cell.accessoryType = .disclosureIndicator
+    }
+    
+    func listList(viewController: ListListViewController, pressedAdd: UIBarButtonItem?) {
+        
+        let alertCon = UIAlertController(title: "Add List", message: nil, preferredStyle: .alert)
+        
+        alertCon.addTextField { (txt) in
+            txt.placeholder = "Name"
+        }
+        
+        alertCon.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in
+            // Do nothing
+        }))
+        
+        alertCon.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
+            
+            guard let name = alertCon.textFields?.first?.text else { return }
+            
+            ServiceProvider().backend.createListWith(name: name).lockView(view: self.view).reportProgress().done({ (list) in
+                self.lists?.append(list)
+                viewController.lists?.append(list)
+            }).cauterize()
+            
+        }))
+            
+        viewController.present(alertCon, animated: true, completion: nil)
+    }
 
+    // MARK: - FilmSearchViewControllerDelegate
+    
+    func filmSelected(sender: FilmSearchViewController?, searchResult result: MediaSearchResult) {
+        
+        guard let vc = sender?.storyboard?.instantiateViewController(withIdentifier: "FilmDetails") as? FilmDetailsViewController else { return }
+        
+        vc.filmPromise = ServiceProvider().imdb.getFilmWith(id: result.id)
+        vc.title = result.title
+        vc.delegate = self
+        
+        sender?.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // MARK: FilmDetailsViewControllerDelegate
 
-
+    func addButtonPressed(sender: FilmDetailsViewController?, button: UIButton?) {
+        
+        let alertCon = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        guard let film = sender?.filmPromise?.value else { return }
+        
+        alertCon.addAction(UIAlertAction(title: "Add to list", style: .default, handler: { [weak self] (action) in
+           
+            guard let vc = self?.storyboard?.instantiateViewController(withIdentifier: "ListList") as? ListListViewController else { return }
+            
+            vc.lists = self?.lists
+            vc.title = "Select List"
+            vc.delegate = ListListViewControllerClosureDelegate(onListSelected: { (vc, list) in
+                ServiceProvider().backend.add(film: film, toList: list).lockView(view: vc.view).reportProgress().done({ (_) in
+                    guard let vc = sender else { return }
+                    vc.navigationController?.popToViewController(vc, animated: true)
+                    try? vc.view.toastViewForMessage("\(film.name ?? "Film") added to \(list.name ?? "list")", title: "Added", image: nil, style: ToastStyle())
+                }).cauterize()
+            })
+            
+            sender?.navigationController?.pushViewController(vc, animated: true)
+        }))
+        
+        alertCon.addAction(UIAlertAction(title: "Add to new list", style: .default, handler: { (action) in
+            // TODO
+        }))
+        
+        alertCon.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (_) in  }))
+        
+        sender?.present(alertCon, animated: true, completion: nil)
+    }
+    
 }
+
+class ListListViewControllerClosureDelegate: ListListViewControllerDelegate {
+    
+    
+    
+    let onListSelected: ((ListListViewController, FilmListReference) -> Void)
+    
+    init(onListSelected: @escaping ((ListListViewController, FilmListReference) -> Void)) {
+        self.onListSelected = onListSelected
+    }
+    
+    func listList(viewController: ListListViewController, selectedList: FilmListReference) {
+        self.onListSelected(viewController, selectedList)
+    }
+    
+    func listList(viewController: ListListViewController, decorateCell: ListListViewControllerTableViewCell, forList: FilmListReference) {
+        // Do nothing
+    }
+    
+    func listList(viewController: ListListViewController, pressedAdd: UIBarButtonItem?) {
+        // Do nothing
+    }
+}
+
+
